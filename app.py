75028b78e6e9afb3f963f29e8a350a9b51ea2ccd
Aqli26 <rosifulaqli13@gmail.com>
2023-06-12 06:06:05 +0700

    ubah

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
+
+SECRET_KEY = "PROJECTFINAL"
+TOKEN_KEY = "mytoken"
+
+MONGODB_CONNECTION_STRING = "mongodb+srv://finalproject1290:Final123@@cluster0.jyp22vw.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.dbfinal
 
 @app.route('/')
 def home():
